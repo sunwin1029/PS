541ad65d0507b7a432ae2c4606b601dc2857166b
sunwin1029 <sunwin1029@gmail.com>
2026-04-18 16:16:19 +0900

    [Gold I] Title: 마법사 상어와 복제, Time: 28 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.cc
+++ 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.cc
@@ -2,7 +2,6 @@
 <head><title>504 Gateway Time-out</title></head>
 <body>
 <center><h1>504 Gateway Time-out</h1></center>
-<hr><center>nginx/1.22.1</center>
 </body>
 </html>
 <!-- a padding to disable MSIE and Chrome friendly error page -->
